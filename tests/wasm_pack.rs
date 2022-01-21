bbbb7a3276a4b9948fbeed8c4bce209ea94811ab
trevyn <trevyn-git@protonmail.com>
2022-01-21 17:16:12 +0300

    clean up tests

--- tests/wasm_pack.rs
+++ tests/wasm_pack.rs
@@ -34,41 +34,25 @@ fn node_test() {
     )
     .unwrap();
 
-    let mut stmt = conn.prepare("SELECT id, name, data FROM person").unwrap();
-    let person_iter = stmt
-        .query_map([], |row| {
+    let person = conn
+        .query_row("SELECT id, name, data FROM person", [], |r| {
             Ok(Person {
-                id: row.get(0)?,
-                name: row.get(1)?,
-                data: row.get(2)?,
+                id: r.get(0).unwrap(),
+                name: r.get(1).unwrap(),
+                data: r.get(2).unwrap(),
             })
         })
         .unwrap();
 
-    let people: Vec<_> = person_iter.collect();
-
     assert_eq!(
-        format!("{:?}", people),
-        r#"[Ok(Person { id: 1, name: "Steven", data: None })]"#
+        format!("{:?}", person),
+        r#"Person { id: 1, name: "Steven", data: None }"#
     );
 
-    let _random: Vec<_> = conn
-        .prepare("SELECT random()")
-        .unwrap()
-        .query_map([], |row| row.get::<_, i64>(0))
-        .unwrap()
-        .collect();
+    let _random: i64 = conn.query_row("SELECT random()", [], |r| r.get(0)).unwrap();
 
-    let now: Vec<_> = conn
-        .prepare(r#"SELECT cast(strftime('%Y') AS decimal)"#)
-        .unwrap()
-        .query_map([], |row| row.get::<_, i32>(0))
-        .unwrap()
-        .collect();
-
-    if let Ok(year) = now[0] {
-        assert!(year >= 2022 && year < 2050);
-    } else {
-        panic!("Expected a year");
-    }
+    let year: i32 = conn
+        .query_row("SELECT cast(strftime('%Y') AS decimal)", [], |r| r.get(0))
+        .unwrap();
+    assert!((2022..2050).contains(&year));
 }
